a9123420358578ff380a14f244f641c0d27492e7
Monika <133509111+Monikagith@users.noreply.github.com>
2026-01-30 13:51:31 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],
+  plugins: [react(), tailwindcss()],
   server: {
-    host: false,
+    host: true,
+    allowedHosts: 'all',
+  },
+  preview: {
+    host: true,
+    allowedHosts: 'all',
+    port: 3000,
   },
 })
